c010e41efae82b9eab303eaea435c7ab4ce4423c
Yuri Romano <103211870+sassogrosso@users.noreply.github.com>
2023-04-14 14:02:34 +0200

    Update face.h

--- face.h
+++ face.h
@@ -1,6 +1,10 @@
-char tasto1;
-    char tasto2;
+    #include <string>
+
+    char g1;
+    char g2;
+    std::string r;
     bool cond1=false;
     bool cond2=false;
+    bool retry=false;
     int num1;
     int num2;
