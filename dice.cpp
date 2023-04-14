2a0fc770839813e131d7f13139627e6e3585681e
Yuri Romano <103211870+sassogrosso@users.noreply.github.com>
2023-04-14 14:02:03 +0200

    Update dice.cpp

--- dice.cpp
+++ dice.cpp
@@ -5,40 +5,50 @@ int main()
     
     srand(time(0));
     std::cout<<std::endl;
-    std::cout<<"                   LANCIO DEL DADO                           \n";
-    std::cout<<"regole del gioco:\n";
-    std::cout<<"per incominciare premere il tasto a della tastiera\n";
-    std::cout<<"se si premera' un tasto di diverso dara' un messaggio di errore\n";
-    std::cout<<"------------giocatore 1 prema il tasto per iniziare: ------------\n";
+    std::cout<<"                   *****    *   *****   *****   *****        \n";
+    std::cout<<"                   *    *       *       *       *            \n";
+    std::cout<<"                   *    *   *   *****   *       *****        \n";
+    std::cout<<"                   *    *   *   *       *       *            \n";
+    std::cout<<"                   *****    *   *****   *****   *****        \n";
+    std::cout<<"rules of the game:\n";
+    std::cout<<"for start the game press the key 's'(s for start) of the keyboard \n";
     do{
-        std::cin>>tasto1;
-        if(tasto1=='a') cond1=true;
-        else std::cout<<"------------tasto sbagliato,riprovare: ------------\n";
-    }while(cond1==false);
-    num1=rand()%6;
+        std::cout<<"------------player 1: ------------\n";
+        do{
+            std::cin>>g1;
+                if(g1=='s') cond1=true;
+                else if(g1!='s') std::cout<<"------------wrong key,retry! ------------\n";
+        }while(!cond1);
+
+    num1=rand()%6+1;
     if(num1==1) std::cout<<"-----\n|   |\n| * |\n|   |\n-----\n"; 
     if(num1==2) std::cout<<"-----\n|*  |\n|   |\n|  *|\n-----\n"; 
     if(num1==3) std::cout<<"-----\n| * |\n| * |\n| * |\n-----\n"; 
     if(num1==4) std::cout<<"-----\n|* *|\n|   |\n|* *|\n-----\n"; 
     if(num1==5) std::cout<<"-----\n|* *|\n| * |\n|* *|\n-----\n"; 
     if(num1==6) std::cout<<"-----\n|* *|\n|* *|\n|* *|\n-----\n"; 
-    std::cout<<"------------giocatore 2 prema il tasto per iniziare: ------------\n";
+    std::cout<<"------------player 2: ------------\n";
     do{
-        std::cin>>tasto2;
-        if(tasto2=='a') cond2=true;
-        else std::cout<<"------------tasto sbagliato,riprovare: ------------\n";
-    }while(cond2==false);
-    num2=rand()%6;    
+        std::cin>>g2;
+        if(g2=='s') cond2=true;
+        else if(g2!='s') std::cout<<"------------wrong key,retry! ------------\n";
+    }while(!cond2);
+    num2=rand()%6+1;    
     if(num2==1) std::cout<<"-----\n|   |\n| * |\n|   |\n-----\n"; 
     if(num2==2) std::cout<<"-----\n|*  |\n|   |\n|  *|\n-----\n"; 
     if(num2==3) std::cout<<"-----\n| * |\n| * |\n| * |\n-----\n"; 
     if(num2==4) std::cout<<"-----\n|* *|\n|   |\n|* *|\n-----\n"; 
     if(num2==5) std::cout<<"-----\n|* *|\n| * |\n|* *|\n-----\n"; 
     if(num2==6) std::cout<<"-----\n|* *|\n|* *|\n|* *|\n-----\n";
-    if(num1>num2) std::cout<<"------------il vincitore e' il giocatore 1------------\n";
-    else if(num1<num2) std::cout<<"------------il vincitore e' il giocatore 2------------\n";
+    if(num1>num2) std::cout<<"------------THE WINNER IS PLAYER 1------------\n";
+    else if(num1<num2) std::cout<<"------------THE WINNER IS PLAYER 2------------\n";
     else {
-         std::cout<<"------------pareggio------------\n";
+         std::cout<<"------------DRAW------------\n";
     }
-
+    std::cout<<'\n';
+    std::cout<<"want to retry? (yes/no)";
+    std::cin>>r;
+    if(r =="no") retry=true;
+    }while(!retry);
+    std::cout<<"GOODBYE";
 }
